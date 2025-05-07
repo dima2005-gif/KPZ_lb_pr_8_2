9e4795c16bad174d32653b77e8e3ff226943ffc3
Dima Velichko <128533732+dima2005-gif@users.noreply.github.com>
2025-05-07 12:40:48 +0300

    Update Entity.ts

--- src/types/Entity.ts
+++ src/types/Entity.ts
@@ -1,7 +1,5 @@
 export interface Entity {
     id: number;
-    name: string;
-    description: string;
-    createdAt: string; // Изменено на string
-    updatedAt: string; // Изменено на string
+    title: string;
+    content: string;
 }
